e49289e4c2e9dae88506fbb44fa2373f06412c9b
Caio-90 <caiomachado3a@gmail.com>
2020-01-12 11:10:24 -0300

    Classe posicao

--- src/aplicacao/Principal.java
+++ src/aplicacao/Principal.java
@@ -1,8 +1,10 @@
 package aplicacao;
 import java.util.Scanner;
+import tabuleiro.Posicao;
 public class Principal {
     public static void main(String[] args) {
-        
+        Posicao p = new Posicao (3,5);
+        System.out.println(p);
     }
     
 }
